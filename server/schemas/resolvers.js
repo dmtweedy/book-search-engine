aadf5ba9cb146c3c73dbb527398210690d693afd
dmtweedy <damon.tweedy@yahoo.com>
2023-09-06 18:47:38 -0500

    created schemas and added mutations and queries files to client.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,61 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (_, __, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-password');
+        return userData;
+      }
+      throw new Error('Not logged in');
+    },
+  },
+  Mutation: {
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new Error('Invalid email or password');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new Error('Invalid email or password');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (_, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (_, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new Error('You must be logged in to save a book.');
+    },
+    removeBook: async (_, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new Error('You must be logged in to remove a book.');
+    },
+  },
+};
+
+module.exports = resolvers;
